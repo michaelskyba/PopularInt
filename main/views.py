97d0be7025051b5d5aec87d4293d9425cf2c2c41
michaelskyba <michaelskyba1411@gmail.com>
2021-04-22 08:31:25 -0400

    made some simple views

--- main/views.py
+++ main/views.py
@@ -4,3 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to PopularInt.")
+
+def register(request):
+    return HttpResponse("Create a free account.")
+
+def login(request):
+    return HttpResponse("Log into your PopularInt account.")
+
+def leaderboard(request):
+    return HttpResponse("Here are the top integers:")
+
+def vote(request):
+    return HttpResponse("Choose an integer to upvote.")
+
+def view_integer(request):
+    return HttpResponse("You're looking at the %s integer." % chosen_integer)
+
+def profile(request):
+    return HttpResponse("You're looking at %s's profile." % user_id)
+
