7bba2d6ac9ce886cc84bebc2da28bc070b99f9f9
michaelskyba <michaelskyba1411@gmail.com>
2021-04-22 09:13:22 -0400

    made index template

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('leaderboard', views.leaderboard, name='leaderboard'),
+    path('vote', views.vote, name='vote'),
+    path('view_integer/<int:chosen_integer>', views.view_integer, name='view_integer'),
+    path('profile/<str:user_id>', views.profile, name='profile'),
 ]
