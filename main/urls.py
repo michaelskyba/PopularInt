aaf3d5f2f981545847362565f4fa2b3b9a7941c5
michaelskyba <michaelskyba1411@gmail.com>
2021-04-23 18:05:10 -0400

    even more views

--- main/urls.py
+++ main/urls.py
@@ -3,13 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # GET
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
-    path('register_submit', views.register_submit, name='register_submit'),
     path('login', views.login, name='login'),
-    path('login_submit', views.login_submit, name='login_submit'),
     path('leaderboard', views.leaderboard, name='leaderboard'),
     path('vote', views.vote, name='vote'),
     path('view_integer/<int:chosen_integer>', views.view_integer, name='view_integer'),
     path('profile/<str:username>', views.profile, name='profile'),
+
+    # POST
+    path('login_submit', views.login_submit, name='login_submit'),
+    path('register_submit', views.register_submit, name='register_submit'),
+    path('vote_submit', views.vote_submit, name='vote_submit'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ from django.http import Http404
 from .models import User
 import datetime
 
-# Create your views here.
+# GET
+
 # Homepage
 def index(request):
     return render(request, 'main/index.html', {})
@@ -13,6 +14,50 @@ def index(request):
 def register(request):
     context = {}
     return render(request, 'main/register.html', context)
+
+# Log in page
+def login(request):
+    context = {}
+    return render(request, 'main/login.html', context)
+
+# Leaderboard page
+def leaderboard(request):
+    context = {}
+    return render(request, 'main/leaderboard.html', context)
+
+# Vote page
+def vote(request):
+    context = {}
+    return render(request, 'main/vote.html', context)
+
+# View an integer
+def view_integer(request, chosen_integer):
+    context = {
+            "chosen_integer": chosen_integer
+            }
+    return render(request, 'main/view_integer.html', context)
+
+# Profile page
+def profile(request, username):
+    # Make sure user exists
+    user = get_object_or_404(User, username_text=username)
+
+    # List of integers
+    integers = []
+    for vote in user.vote_set.all():
+        integers.append(vote)
+
+    # rest of the context
+    context = {
+            "username": user.username_text,
+            "register": user.registration_date,
+            "password": user.password_text,
+            "integers": integers
+            }
+
+    return render(request, 'main/profile.html', context)
+
+# POST
 
 # Register POST submission
 def register_submit(request):
@@ -62,49 +107,13 @@ def register_submit(request):
         context = {"message": "Registered successfully."}
         return render(request, 'main/register.html', context)
 
-# Log in page
-def login(request):
-    context = {}
-    return render(request, 'main/login.html', context)
-
+# Log in POST submission
 def login_submit(request):
     context = {}
     return render(request, 'main/login.html', context)
 
-# Leaderboard page
-def leaderboard(request):
+# Vote POST submission
+def vote_submit(request):
     context = {}
-    return render(request, 'main/leaderboard.html', context)
-
-# Vote page
-def vote(request):
-    context = {}
-    return render(request, 'main/vote.html', context)
-
-# View an integer
-def view_integer(request, chosen_integer):
-    context = {
-            "chosen_integer": chosen_integer
-            }
-    return render(request, 'main/view_integer.html', context)
-
-# Profile page
-def profile(request, username):
-    # Make sure user exists
-    user = get_object_or_404(User, username_text=username)
-
-    # List of integers
-    integers = []
-    for vote in user.vote_set.all():
-        integers.append(vote)
-
-    # rest of the context
-    context = {
-            "username": user.username_text,
-            "register": user.registration_date,
-            "password": user.password_text,
-            "integers": integers
-            }
-
-    return render(request, 'main/profile.html', context)
+    return render(request, 'main/post.html', context)
 
