4b85e449e23169de7943ca69de7e7854bb9dedbb
michaelskyba <michaelskyba1411@gmail.com>
2021-04-23 13:19:03 -0400

    Changed a lot of views

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
+    path('register_submit', views.register_submit, name='register_submit'),
     path('login', views.login, name='login'),
+    path('login_submit', views.login_submit, name='login_submit'),
     path('leaderboard', views.leaderboard, name='leaderboard'),
     path('vote', views.vote, name='vote'),
     path('view_integer/<int:chosen_integer>', views.view_integer, name='view_integer'),
--- main/views.py
+++ main/views.py
@@ -4,31 +4,60 @@ from django.http import Http404
 from .models import User
 
 # Create your views here.
+# Homepage
 def index(request):
     return render(request, 'main/index.html', {})
 
+# Register page
 def register(request):
     context = {}
     return render(request, 'main/register.html', context)
 
+# Register POST submission
+def register_submit(request):
+    context = {"message": "Successfully registered."}
+
+    try:
+        if request.POST["password_confirm"] != request.POST["password"]:
+            context = {"message": "Passwords do not match."}
+
+        if not request.POST["password"]:
+            context = {"message": "You need to provide a password."}
+
+        if not request.POST["username"]:
+            context = {"message": "You need to provide a username."}
+    except MultiValueDictKeyError:
+        context = {"message": "tf are you doing?"}
+
+    return render(request, 'main/register.html', context)
+
+# Log in page
 def login(request):
     context = {}
     return render(request, 'main/login.html', context)
 
+def login_submit(request):
+    context = {}
+    return render(request, 'main/login.html', context)
+
+# Leaderboard page
 def leaderboard(request):
     context = {}
     return render(request, 'main/leaderboard.html', context)
 
+# Vote page
 def vote(request):
     context = {}
     return render(request, 'main/vote.html', context)
 
+# View an integer
 def view_integer(request, chosen_integer):
     context = {
             "chosen_integer": chosen_integer
             }
     return render(request, 'main/view_integer.html', context)
 
+# Profile page
 def profile(request, username):
     # Make sure user exists
     user = get_object_or_404(User, username_text=username)
